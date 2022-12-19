efb211abaf8b6639e24fcbd9a7a7a3ef79e94fc6
JI-Maina <irungujmaina@gmail.com>
2022-12-19 12:43:57 -0800

    Not yet uhuru

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# A function that prints the first x elements of a list and only integers
+# A function that prints the first x elements of a list & only integers
 # Returns the real number of integers printed
 
 def safe_print_list_integers(my_list=[], x=0):
