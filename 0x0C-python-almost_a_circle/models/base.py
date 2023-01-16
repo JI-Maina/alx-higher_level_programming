05ba5bf175c0b4ae227e97fc3412a154f071b094
JI-Maina <irungujmaina@gmail.com>
2023-01-16 12:06:40 -0800

    File to instances

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -49,7 +49,45 @@ class Base:
             if list_objs is None:
                 f.write("[]")
             else:
-#     dict_str = [instance.to_dictionary() for instance in list_objs]
                 for instance in list_objs:
                     dict_str = [instance.to_dictionary()]
                     f.write(Base.to_json_string(dict_str))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list of the JSON string representation json_string.
+        Args:
+            json_string (str): a string representing a list of dictionaries.
+        """
+        if json_string is None or json_string == []:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes already set.
+
+        Args:
+            dictionary (dict): key/word pair of attributes.
+        """
+        from models.rectangle import Rectangle
+        from models.square import Square
+
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                r1 = cls(1, 1)
+            else:
+                r1 = cls(1)
+            r1.update(**dictionary)
+            return r1
+
+    @classmethod
+    def load_from_file(cls):
+        """Returns a list of instances."""
+        filename = str(cls.__name__) + ".json"
+        try:
+            with open(filename, "r") as jsonfile:
+                list_dicts = Base.from_json_string(jsonfile.read())
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
