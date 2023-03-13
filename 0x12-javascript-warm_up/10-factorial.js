c1d67150c002a84c61f8ef5ab7843b5807610085
JI-Maina <irungujmaina@gmail.com>
2023-03-13 11:23:53 -0700

    Factorial changes

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,21 +1,16 @@
 #!/usr/bin/node
 /* computes and prints a factorial */
 
-/* import process module */
-const process = require('process');
-
-const num = parseInt(process.argv[2]);
-
-if (!num) {
-  console.log(1);
-} else {
-  function calcFactorial (num) {
-    if (num <= 1) {
-      return 1;
-    } else {
-      return num * calcFactorial(num - 1);
-    }
+function calcFactorial (num) {
+  if (!parseInt(num)) {
+    return 1;
   }
-  const fact = calcFactorial(num);
-  console.log(fact);
+
+  if (num <= 1) {
+    return 1;
+  } else {
+    return num * calcFactorial(num - 1);
+  }
 }
+
+console.log(calcFactorial(process.argv[2]));
