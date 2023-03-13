1454de23954c2cb02a5482e1fb628568fc419cf0
JI-Maina <irungujmaina@gmail.com>
2023-03-13 08:50:08 -0700

    An Integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/* Import process module */
+const process = require('process');
+
+const num = parseInt(process.argv[2]);
+
+if (num) {
+  console.log('My number: ' + num);
+} else {
+  console.log('Not a number');
+}
