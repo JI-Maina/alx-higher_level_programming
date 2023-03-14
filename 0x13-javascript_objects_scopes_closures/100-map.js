8ed5d38cacc3b9afa5e93c1f4ecb005ecee89efc
JI-Maina <irungujmaina@gmail.com>
2023-03-14 11:19:01 -0700

    Factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,14 +2,12 @@
 /* script that imports an array and computes a new array */
 
 const list = require('./100-data').list;
-const len = list.length;
+
 let newList = [];
 
-newList = list.map(item => item);
-
-for (let i = 0; i < len; i++) {
-  newList[i] = newList[i] * i;
-}
+newList = list.map((item, index) => {
+  return item * index;
+});
 
 console.log(list);
 console.log(newList);
