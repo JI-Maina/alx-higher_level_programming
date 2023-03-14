7e50742c018c207f70833b81434d4a8a17af2414
JI-Maina <irungujmaina@gmail.com>
2023-03-14 06:58:58 -0700

    Rectangle #1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* class Rectangle that defines a rectangle with instance attributes w & h */
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
