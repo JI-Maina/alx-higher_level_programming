d7b2beec91c501e205ac97b5c87c329b1f297359
JI-Maina <irungujmaina@gmail.com>
2023-03-31 10:12:37 -0700

    POST an email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""takes in a URL and an email, sends a POST request to the passed URL with the
+email as a parameter, and displays the body of the response (decoded in utf-8)
+"""
+
+if __name__ == "__main__":
+    from urllib.request import urlopen, Request
+    from sys import argv
+
+    data1 = f"email={argv[2]}".encode('utf8')
+
+    # req = Request(argv[1], data=data1, method='POST')
+
+    with urlopen(argv[1], data1) as response:
+        resp = response.read().decode('utf-8')
+
+    print(resp)
