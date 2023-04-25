d3805b7662f606e1a0701f0faf5803e50c6550ac
JI-Maina <irungujmaina@gmail.com>
2023-04-25 20:39:59 +0000

    How many completed?

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+/* computes the number of tasks completed by user id */
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const userIds = [];
+    const todos = JSON.parse(body);
+
+    // grab all the user ids
+    for (const todo of todos) {
+      if (!userIds.includes(todo.userId)) {
+        userIds.push(todo.userId);
+      }
+    }
+
+    const compTask = {};
+
+    // compute completed tasks and update obj
+    for (const id of userIds) {
+      let count = 0;
+      for (const todo of todos) {
+        if (todo.userId === id) {
+          if (todo.completed) {
+            count += 1;
+          }
+        }
+        compTask[id] = count;
+      }
+    }
+    console.log(compTask);
+  }
+});
